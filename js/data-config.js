61a0650cb4ec5d95922637ed77b122c7df5c2d2b
Hugh_ <112853932+Hugh-888@users.noreply.github.com>
2026-06-14 18:31:13 +0800

    Update data-config.js

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'Hugh-888',
 
     /**
      * GitHub repository name
